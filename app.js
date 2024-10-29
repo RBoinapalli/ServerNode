8d1e766f18b2ee87b37d9f0c423b994018ce6978
Rithika Boinapalli <rithika.boinapalli@gmail.com>
2024-10-29 10:25:23 -0400

    Test3

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var http = require("http");
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 3001;
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
-  })
-  .listen(port);
-http
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
